d9c2367b1f79b2519656e4a1ef1ff50591ce17c3
Shiva Raj Mishra <shivarajmishra@gmail.com>
2022-06-29 16:38:25 +1000

    added inputs for mean and sd

--- BetaCheck.py
+++ BetaCheck.py
@@ -2,6 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import beta
 
+"""mean=[np.log(float(input("what is your mean value?")))]"""
+"""Alternatively, specify the value of mean here"""
+mean=np.log(1.58)
+print("Mean", mean)
+
+def getSd(UCI, LCI):
+    sigma=(np.log(UCI)-np.log(LCI))/3.96
+    return sigma
+
+sd=getSd(1.72,1.44)
+print("Standard Deviation:", sd)
+
 # estimate parameters of beta dist.
 def getAlphaBeta(mu, sigma):
     alpha = mu**2 * ((1 - mu) / sigma**2 - 1 / mu)
@@ -10,7 +22,9 @@ def getAlphaBeta(mu, sigma):
 
     return [alpha, beta]
 
-a1=getAlphaBeta(0.520323374, 0.0418978878)  # {e.g. mean: 111 mmHg, std: 10}
+a1=getAlphaBeta(mean, sd)  
+"""e.g. mean: 0.520323374, std: 0.0418978878 for RR 1.52 (95% CI: 1.44-1.72)"""
+
 a=a1[0]
 print('Alpha:',a1[0])
 b=a1[1]
